a35a9aafe694b7ab27537abb599f241b57d129e4
GRACENOBLE <gracenoble72@gmail.com>
2025-06-08 12:26:00 +0300

    feat: update global styles and add dark mode support with new color variables
    feat: integrate Header component into layout for improved navigation
    
    refactor: simplify Home page structure and enhance layout
    
    feat: add components.json for component configuration and aliases
    
    chore: configure drizzle ORM for PostgreSQL database connection
    
    feat: create Dashboard page with Container component for layout
    
    feat: implement reusable Container component for consistent layout
    
    feat: develop Header component with navigation links and user avatar
    
    feat: create Button component with variant support for consistent styling
    
    chore: set up database connection and schema for users table
    
    feat: add utility functions for class name management
    
    chore: implement middleware for Clerk authentication

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import Container from "@/components/common/container";
+
+const page = () => {
+  return <Container className="min-h-[80vh] grid place-items-center">This is the Dashboard</Container>;
+};
+
+export default page;
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import Container from "../common/container";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "../ui/button";
+
+const Header = () => {
+  return (
+    <header className="border-b py-4">
+      <Container className={cn("flex justify-between items-center")}>
+        <Link href={"/"} className="text-2xl font-semibold">
+          Clerk Auth
+        </Link>
+        <div className="flex items-center gap-20">
+          <div>
+            {[
+              { href: "/", label: "home" },
+              { href: "/dashboard", label: "dashboard" },
+              //   { href: "/bookmarks", label: "Bookmarks" },
+            ].map(({ href, label }) => (
+              <Link
+                key={href}
+                href={href}
+                className={cn(
+                  " min-w-28",
+                  buttonVariants({ variant: "ghost" })
+                )}
+              >
+                {label}
+              </Link>
+            ))}
+          </div>
+          <div className="aspect-square p-2 bg-muted rounded-full">GN</div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
